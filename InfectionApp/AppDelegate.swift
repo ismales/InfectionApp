d3f15b8969b97b93f3a5d7d5bc6fe58375d24323
Ismail Aleskerov <manofsteel1992@gmail.com>
2023-05-04 16:26:23 +0300

    init commit

---
+++ InfectionApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  InfectionApp
+//
+//  Created by Сманчос on 04.05.2023.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ InfectionApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  InfectionApp
+//
+//  Created by Сманчос on 04.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+    }
+}
